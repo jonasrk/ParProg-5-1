48f854cec7eacfce9289ccfa63448518fbaf2e4b
Jonas <jonas@jonass-mbp.fritz.box>
2014-01-15 21:01:03 +0100

    initial commit xCode project

---
+++ mpi_hello/mpi_hello/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mpi_hello
+//
+//  Created by Jonas on 15/01/14.
+//  Copyright (c) 2014 Jonas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
